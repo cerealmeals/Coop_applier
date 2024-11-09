ec4c708fe84ddea94c9fd9cc55291c65cd77f4ff
Sam Nygard <samnygard@gmail.com>
2024-11-08 17:22:41 -0800

    cover letter creation to pdf, no smartness yet

--- createCoverLetter.py
+++ createCoverLetter.py
@@ -1,19 +1,22 @@
-from python_docx_replace import docx_replace
 from docx import Document
+from docx2pdf import convert
 import datetime
 import os
 
 current = os.getcwd()
-current = os.path.join(current, 'Jobs')
+jobs = os.path.join(current, 'Jobs')
+
 
 def createCoverLetter(file):
 
     data = read_file(file)
     # # get your document using python-docx
-    doc = Document("Cover_letter_test.docx")
+    cover = os.path.join('cover_letters', "Cover_letter_test.docx")
+    doc = Document(cover)
 
     date = datetime.datetime.now()
-    replace_word = {'[Date]': date.strftime("%B %b, %Y"), '[Company]': 'ABC'}
+    replace_word = {'[Date]': date.strftime("%B %b, %Y")}
+    replace_word.update(read_file(file))
 
     for word in replace_word:
         for p in doc.paragraphs:
@@ -21,18 +24,40 @@ def createCoverLetter(file):
                 p.text = p.text.replace(word, replace_word[word])
 
     
-    
-    doc.save(file + ".docx")
+    save_path = os.path.join('cover_letters', file +'.docx')
+    doc.save(save_path)
+    convert(save_path)
 
 def read_file(file):
 
+    result = {}
     try:
-        f = open(os.path.join(current, file), 'r+')
-        data = f.read()
+        f_jobs = open(os.path.join(jobs, file), 'r+')
+        data = f_jobs.read()
     except Exception as e:
         print('file Error trying to read data', str(e))
 
-    Company_start = '#\nAddress Line One: '.find()
+
+    result.update(getLine(data, '#\nAddress Line One: ', '[Address]'))
+    result.update(getLine(data, '#\nCity: ', '[City]'))
+    result.update(getLine(data, '#\nProvince / State: ', '[Province]')) # need to translate to letters
+    result.update(getLine(data, '#\nPostal Code / Zip Code: ', '[Postal]'))
+    result.update({'[Title]': file})
+    result.update(getLine(data, '#\nOrganization: ', '[Company]'))
+
+    
+    
+    return result
+    
+
+def getLine(data, line, type):
+    start = data.find(line)
+    if start > -1:
+        start += len(line)
+        end = data.find('#', start)
+        string = data[start:end]
+        return {type:string}
+    return
 
 
 createCoverLetter('123757 - Software Developer')
